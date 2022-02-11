ddf112ae5e1ef1b9e82553b9ccb11ef648c70bc9
Ivo Limmen <ivo@limmen.org>
2022-02-11 20:03:22 +0100

    Added email to the team settings.

--- shared/model/src/main/java/org/limmen/zenodotus/project/model/Team.java
+++ shared/model/src/main/java/org/limmen/zenodotus/project/model/Team.java
@@ -8,16 +8,21 @@ public class Team {
 
   private String name;
 
+  private String email;
+
   @JsonPOJOBuilder(withPrefix = "")
   public static class Builder {
 
     private String name;
 
+    private String email;
+
     public Builder() {
     }
 
-    Builder(String name) {
+    Builder(String name, String email) {
       this.name = name;
+      this.email = email;
     }
 
     public Builder name(String name) {
@@ -25,17 +30,26 @@ public class Team {
       return Builder.this;
     }
 
-    public Team build() {
+    public Builder email(String email) {
+      this.email = email;
+      return Builder.this;
+    }
 
+    public Team build() {
       return new Team(this);
     }
   }
 
   private Team(Builder builder) {
     this.name = builder.name;
+    this.email = builder.email;
   }
 
   public String getName() {
     return this.name;
+  }
+
+  public String getEmail() {
+    return email;
   }
 }
