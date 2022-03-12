b9cfa27d1fcb3c2c3b1d830c723867b150b72748
Ivo Limmen <ivo@limmen.org>
2022-03-12 10:59:14 +0100

    Updated the documentation.

--- docgen/domain/src/main/java/org/limmen/docgen/domain/IndexGenerator.java
+++ docgen/domain/src/main/java/org/limmen/docgen/domain/IndexGenerator.java
@@ -6,7 +6,10 @@ import java.nio.file.Path;
 import org.limmen.docgen.domain.index.SearchIndexGenerator;
 
 public interface IndexGenerator {
+
   SearchIndexGenerator getSearchIndexGenerator();
+
   void addNewLink(Path targetFile);
+  
   void generate() throws IOException;  
 }
